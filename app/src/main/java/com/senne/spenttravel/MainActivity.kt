c30b43fba459a44a1bc4136ad8ca8d2da3950c54
Moreira <im.senne2017@gmail.com>
2021-02-10 20:42:20 +0000

    Layout OK

--- app/src/main/java/com/senne/spenttravel/MainActivity.kt
+++ app/src/main/java/com/senne/spenttravel/MainActivity.kt
@@ -2,10 +2,25 @@ package com.senne.spenttravel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn : Button = findViewById(R.id.buttonCalculate)
+        btn.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if(id == R.id.buttonCalculate) {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+
     }
 }
